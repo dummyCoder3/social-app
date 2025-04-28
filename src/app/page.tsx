0c8da6912aa24ba7c01de6c68b2b8a9a905a98d3
adminHamid <hamidazimi2003@gmail.com>
2025-04-28 15:39:06 +0500

    Add user interaction features with FollowButton and WhoToFollow components
    - Introduced FollowButton component for following/unfollowing users with feedback notifications.
    - Added WhoToFollow component to suggest random users to follow.
    - Implemented getRandomUsers and toggleFollow functions in user actions for user management.
    - Updated Home page layout to include WhoToFollow and PostCard components for enhanced user experience.
    - Added avatar.png as a default image for user profiles.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import CreatePost from "@/components/CreatePost";
 import { currentUser } from "@clerk/nextjs/server";
+
+import WhoToFollow from "@/components/WhoToFollow";
+import CreatePost from "@/components/CreatePost";
+import PostCard from "@/components/PostCard";
 
 export default async function Home() {
   const user = await currentUser();
+  const posts = await getPosts();
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
       <div className="hidden sticky top-20 lg:block lg:col-span-4">
-        WhoToFollow??
+        <WhoToFollow />
       </div>
     </div>
   );
